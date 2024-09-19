6f239e22d15fc629fefd0c138372b6d39e210ba3
L-Durand <loic.durand.perso@gmail.com>
2024-09-20 01:23:50 +0200

    Ajout du swagger

--- src/configurations/express.js
+++ src/configurations/express.js
@@ -5,12 +5,15 @@ import authRoutes from '../routes/auth.js'
 import clientRoutes from '../routes/client.js'
 import orderRoutes from '../routes/order.js'
 import productRoutes from '../routes/product.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
 
 /**
  * Express configuration
  */
 
 export async function configure (app) {
+    const swaggerFile = JSON.parse(fs.readFileSync('./swagger-output.json', 'utf-8'));
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
@@ -18,6 +21,7 @@ export async function configure (app) {
     app.use('/client', clientRoutes)
     app.use('/order', orderRoutes)
     app.use('/product', productRoutes)
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
     app.use(errorHandler)
     console.log('Express configured.')
 }
